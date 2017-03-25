f60d0dadd524b7bf05d5ca89f0d3c91e807762e1
Zephyr <lococeq.1985@gmail.com>
2017-03-25 17:26:22 -0500

    Added new test for the resourceImplementOptionsMethod function

--- test/resource-implement-options-method.spec.js
+++ test/resource-implement-options-method.spec.js
@@ -2,7 +2,8 @@
 'use strict';
 
 const ava = require('ava'),
-      restHalTestTools = require('../');
+  restHalTestTools = require('../'),
+  superTest = require('supertest');
 
 ava("resourceImplementOptionsMethod fail if agent not provided", (t) => {
   t.throws(() => {
@@ -10,4 +11,14 @@ ava("resourceImplementOptionsMethod fail if agent not provided", (t) => {
 
     restHalTestTools.resourceImplementOptionsMethod(testContext);
   }, 'Cannot read property \'options\' of undefined');
+});
+
+ava("resourceImplementOptionsMethod fail if resource not provided", (t) => {
+  t.throws(() => {
+    let testContext = { context: {
+      agent: superTest('http://some.api')
+    } };
+
+    restHalTestTools.resourceImplementOptionsMethod(testContext);
+  }, 'undefined');
 });
