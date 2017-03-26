35d03390d2c01a2ba28a069995c8e91a8363863e
Zephyr <lococeq.1985@gmail.com>
2017-03-25 20:49:42 -0500

    Updated code to make coverage report more real

--- src/resource-implement-options-method.ts
+++ src/resource-implement-options-method.ts
@@ -7,6 +7,7 @@ import * as restHalTestContext from "./rest-hal-test-context";
 /** Check if the resource under test implement the OPTIONS method */
 export function resourceImplementOptionsMethod(
         t: restHalTestContext.IRestHalTestContext): superTest.Test {
-    return t.context.agent.options(t.context.resource)
-        .expect(200);
+    let request =  t.context.agent.options(t.context.resource);
+
+    return request.expect(200);
 }
