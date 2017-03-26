48360755fb73374bb95058348ad5d363cb767b81
Zephyr <lococeq.1985@gmail.com>
2017-03-25 22:44:04 -0500

    Simplified tests for the resourceImplementOptionsMethod function

--- test/resource-implement-options-method.spec.js
+++ test/resource-implement-options-method.spec.js
@@ -6,21 +6,17 @@ const ava = require('ava'),
   superTest = require('supertest');
 
 ava("resourceImplementOptionsMethod fail if agent not provided", (t) => {
-  let testContext = { context: {} };
-
   t.throws(
-    restHalTestTools.resourceImplementOptionsMethod.bind(null, testContext),
+    restHalTestTools.resourceImplementOptionsMethod.bind(null, t),
     'Cannot read property \'options\' of undefined'
   );
 });
 
 ava("resourceImplementOptionsMethod fail if resource not provided", (t) => {
-  let testContext = { context: {
-    agent: superTest('http://some.api')
-  } };
+  t.context.agent = superTest('http://some.api');
 
   t.throws(
-    restHalTestTools.resourceImplementOptionsMethod.bind(null, testContext),
+    restHalTestTools.resourceImplementOptionsMethod.bind(null, t),
     'undefined'
   );
 });
@@ -31,12 +27,10 @@ ava("resourceImplementOptionsMethod fail if status is not 200", (t) => {
     res.end();
   }
 
-  let testContext = { context: {
-    resource: "/",
-    agent: superTest(handler)
-  } };
+  t.context.resource = "/";
+  t.context.agent = superTest(handler);
 
-  restHalTestTools.resourceImplementOptionsMethod(testContext)
+  restHalTestTools.resourceImplementOptionsMethod(t)
     .end((res) => { t.ifError(res); });
 });
 
@@ -46,11 +40,8 @@ ava("resourceImplementOptionsMethod succeed if status is 200", (t) => {
     res.end();
   }
 
-  let testContext = { context: {
-    resource: "/",
-    agent: superTest(handler)
-  } };
+  t.context.resource = "/";
+  t.context.agent = superTest(handler);
 
-  restHalTestTools.resourceImplementOptionsMethod(testContext)
-    .end((res) => { t.is(res, null); });
+  return restHalTestTools.resourceImplementOptionsMethod(t);
 });
