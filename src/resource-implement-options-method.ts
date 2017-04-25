77979edd839586ffaaf18600506ffda5a8fb2b7c
Zephyr <lococeq.1985@gmail.com>
2017-04-25 15:46:26 -0500

    Fixed alignment on resource-implement-options-method source file

--- src/resource-implement-options-method.ts
+++ src/resource-implement-options-method.ts
@@ -6,8 +6,8 @@ import * as restHalTestContext from "./rest-hal-test-context";
 
 /** Check if the resource under test implement the OPTIONS method */
 export function resourceImplementOptionsMethod(
-        t: restHalTestContext.IRestHalTestContext): superTest.Test {
-    let request =  t.context.agent.options(t.context.resource);
+    t: restHalTestContext.IRestHalTestContext): superTest.Test {
+  let request =  t.context.agent.options(t.context.resource);
 
   return request.expect((res) => {
     t.true(res.status >= 200 && res.status < 400);
