c7802379f06de8c793aa6a10a68c17b8542ae9ad
Zephyr <lococeq.1985@gmail.com>
2017-06-22 11:52:15 -0500

    Added new test suite for a new test feature

---
+++ src/resource-get-method-implement-server-driven-content-negotiation.spec.ts
@@ -0,0 +1,22 @@
+
+import ava from "ava";
+import * as express from "express";
+import * as superTest from "supertest";
+
+import * as resourceGETMethodImplementServerDrivenContentNegotiation from "./resource-get-method-implement-server-driven-content-negotiation";
+
+ava.beforeEach((t) => {
+    const app = express();
+
+    // provide an endpoint handler for the GET method
+    app.get("/", (_, res) => {
+      res.format({
+        default: () => res.status(406).end(),
+      });
+    });
+
+    t.context.agent = superTest(app);
+    t.context.resource = "/";
+});
+
+ava(resourceGETMethodImplementServerDrivenContentNegotiation.resourceGETMethodImplementServerDrivenContentNegotiation);
