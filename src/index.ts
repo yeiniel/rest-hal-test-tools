e51b99f9fe666d889eb89dac0f656a2f95b235ac
Zephyr <lococeq.1985@gmail.com>
2017-06-22 12:11:56 -0500

    Exported new test

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@
 export * from "./resource-implement-get-method";
 export * from "./resource-implement-options-method";
 export * from "./resource-options-method-allow-get";
+export * from "./resource-get-method-implement-server-driven-content-negotiation";
