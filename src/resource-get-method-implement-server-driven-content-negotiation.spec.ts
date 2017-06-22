51f47e7bcc657d00074f292cb6d864ef0574f903
Zephyr <lococeq.1985@gmail.com>
2017-06-22 12:08:05 -0500

    Fixed linting problem

--- src/resource-get-method-implement-server-driven-content-negotiation.spec.ts
+++ src/resource-get-method-implement-server-driven-content-negotiation.spec.ts
@@ -3,7 +3,8 @@ import ava from "ava";
 import * as express from "express";
 import * as superTest from "supertest";
 
-import * as resourceGETMethodImplementServerDrivenContentNegotiation from "./resource-get-method-implement-server-driven-content-negotiation";
+import * as resourceGETMethodImplementServerDrivenContentNegotiation
+  from "./resource-get-method-implement-server-driven-content-negotiation";
 
 ava.beforeEach((t) => {
     const app = express();
